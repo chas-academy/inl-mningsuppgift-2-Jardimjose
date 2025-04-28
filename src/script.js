9634226c61688c9e23c64d78cc228da94ef72da1
José Jardim <jardim.jose@outlook.com>
2025-04-28 18:35:38 +0200

    finished

--- src/script.js
+++ src/script.js
@@ -1,30 +1,43 @@
 // Definera array av namn
-let people;
+let people = [
+  "John",
+  "Mia", 
+  "Karl",
+  "Anna", "Johan", "Lisa", "Erik"
+];
 
 
 export function initSearch(people) { // rör ej denna rad
 
   // Hämta HTML-element
-  
+  let searchInput = document.getElementById("searchInput");
+  let searchBtn = document.getElementById("searchBtn");
+  let resultDisplay = document.getElementById("resultDisplay");
   // Lägg till en klickhändelse på "Sök"-knappen
   searchBtn.addEventListener("click",() => {
   
     // 1. Hämta texten i searchInput.value
-    
+    const name = searchInput.value; 
     // 2. Skapa en boolean för found
-    
+    let found = false; 
     // 3. Loopa igenom people-arrayen
-    
-    // 4. Jämför texten med varje namn i arrayen
-    
-    // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
-    
+    for (let person of people)
+    {
+      // 4. Jämför texten med varje namn i arrayen
+      if(person === name)
+      {
+        // 5. Om du hittar en match, skriv ut "Namn hittades!" i resultDisplay, ändra found till true
+        resultDisplay.innerHTML = "Namn hittades: " + name;
+        found = true;
+      }
+    }
     // 6. Om du efter loopen inte hittat något, skriv ut "Namn hittades inte." och gör found till false igen.
-    
+    if(!found)
+    {
+      resultDisplay.innerHTML = "Namn hittades inte.";
+    }
     });
 }
-
-
 
 // rör ej dessa rader
 document.addEventListener("DOMContentLoaded", () => {
